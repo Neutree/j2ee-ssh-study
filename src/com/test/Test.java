d50a7e891e19b69df37f9d4bd46cebb7c0cfddff
Neutree <czd666666@gmail.com>
2015-10-26 14:32:48 +0800

    add test file

---
+++ src/com/test/Test.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("testing......\n");
+		
+		
+		System.out.println("testing......\n");
+	}
+
+}
