33a3de8392780bb776a173afd22600cf02e96ff0
Neutree <czd666666@gmail.com>
2015-11-04 20:21:22 +0800

    add action extends

--- src/com/action/UserAction.java
+++ src/com/action/UserAction.java
@@ -1,7 +1,14 @@
 package com.action;
 
+import com.opensymphony.xwork2.ActionSupport;
 
-public class UserAction {
+
+public class UserAction extends ActionSupport{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1572209882148568525L;
+
 	public String execute() throws Exception {
 		// TODO Auto-generated method stub
 		return "success";
